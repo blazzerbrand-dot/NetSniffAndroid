1538f5844999d082c6000b7b72004596759a7fd7
karthik449213 <karthikpiinasi@gmail.com>
2026-05-23 00:15:42 +0530

    we sperately added the appconstants

---
+++ app/src/main/java/com/example/netsniffandroid/core/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.example.netsniffandroid.core.utils
+
+object AppConstants {
+    const val TAG = "NETSNIFF"
+    const val MAX_PACKET_SIZE = 65536
+    const val PACKET_BUFFER_POOL_SIZE = 256
+    const val NATIVE_LIBRARY = "netsniffandroid"
+    const val VPN_MTU = 1500
+
+}
